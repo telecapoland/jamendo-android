cc3f74ca374b07cb28d112cc7bd04e414e0d4d77
Bartosz Cichosz <Bartosz.Cichosz@teleca.com>
2011-03-24 13:32:52 +0100

    IllegalArgumentException in forgetReceiverDispatcher() fix
    This is a known Android issue.
    http://code.google.com/p/android/issues/detail?id=6191
    
    On Android 2.1, 2.2 and 2.3 VievFlipper causes crashes while
    phone orientation is changing rapidly.
    The workaround proposed in above thread has been aplied.

---
+++ src/com/teleca/jamendo/util/FixedViewFlipper.java
@@ -0,0 +1,37 @@
+package com.teleca.jamendo.util;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ViewFlipper;
+
+public class FixedViewFlipper extends ViewFlipper {
+
+	public FixedViewFlipper(Context context) {
+		super(context);
+	}
+
+	public FixedViewFlipper(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	protected void onDetachedFromWindow() {
+
+		int apiLevel = Build.VERSION.SDK_INT;
+
+		if (apiLevel >= 7 && apiLevel < 10) {
+			try {
+				super.onDetachedFromWindow();
+			} catch (IllegalArgumentException e) {
+				Log.w("Jamendo", "Android project  issue 6191  workaround.");
+				/* Quick catch and continue on api level 7, the Eclair 2.1 */
+			} finally {
+				super.stopFlipping();
+			}
+		} else {
+			super.onDetachedFromWindow();
+		}
+	}
+}
