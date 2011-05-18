bf2fe7459f2e4fcff9775d3b3a8c436757309733
Marcin Gil <marcin.gil@teleca.com>
2011-05-18 15:00:35 +0200

    Fixed 1.5 support - SDK2.2 breaks compatibility for 1.5.

--- src/com/teleca/jamendo/util/FixedViewFlipper.java
+++ src/com/teleca/jamendo/util/FixedViewFlipper.java
@@ -16,22 +16,22 @@ public class FixedViewFlipper extends ViewFlipper {
 		super(context, attrs);
 	}
 
-	@Override
-	protected void onDetachedFromWindow() {
-
-		int apiLevel = Build.VERSION.SDK_INT;
-
-		if (apiLevel >= 7) {
-			try {
-				super.onDetachedFromWindow();
-			} catch (IllegalArgumentException e) {
-				Log.w("Jamendo", "Android project  issue 6191  workaround.");
-				/* Quick catch and continue on api level 7, the Eclair 2.1 */
-			} finally {
-				super.stopFlipping();
-			}
-		} else {
-			super.onDetachedFromWindow();
-		}
-	}
+//	@Override
+//	protected void onDetachedFromWindow() {
+//
+//		int apiLevel = Build.VERSION.SDK_INT;
+//
+//		if (apiLevel >= 7) {
+//			try {
+//				super.onDetachedFromWindow();
+//			} catch (IllegalArgumentException e) {
+//				Log.w("Jamendo", "Android project  issue 6191  workaround.");
+//				/* Quick catch and continue on api level 7, the Eclair 2.1 */
+//			} finally {
+//				super.stopFlipping();
+//			}
+//		} else {
+//			super.onDetachedFromWindow();
+//		}
+//	}
 }
